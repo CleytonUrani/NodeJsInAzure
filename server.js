ee6ce7de5f5c06126b1e2c90d4056ecbffe22b99
CleytonUrani <CleytonUrani@users.noreply.github.com>
2016-11-28 12:34:58 -0200

    trocando .path para .url
    Na comparação da req. para tomada de decisão do destino do usuário.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    if(req.path === '/sobre'){
+    if(req.url === '/sobre'){
 		res.end('<em>Sobre</em> Node.js');
 	}
 	else{
