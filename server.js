78af14616d969f413bc12828bcc5f5c53fb78a17
Cleyton Urani <CleytonUrani@users.noreply.github.com>
2017-03-17 11:14:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var http = require('http');
 var port = process.env.port || 1337;
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     if(req.url === '/sobre'){
 		res.end('Sobre....');
 	}
 	else{
-		res.end('<p>Hello World.<p>');
+		res.end('<p>Hello World</p>');
 	}
 }).listen(port);
