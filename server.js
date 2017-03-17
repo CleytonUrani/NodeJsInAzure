6dda65a702ffee6b38a89a8026d29a6996d6287a
Cleyton Urani <CleytonUrani@users.noreply.github.com>
2017-03-17 11:10:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 var http = require('http');
 var port = process.env.port || 1337;
+
+require("jsdom").env("", function(err, window) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+ 
+    var $ = require("jquery")(window);
+});
+
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     if(req.url === '/sobre'){
-		res.end('Sobre....Max');
+		res.end('Sobre....');
 	}
 	else{
-		res.end('Hello World.\n');
+		res.end('<p>Hello World.<p>');
 	}
 }).listen(port);
