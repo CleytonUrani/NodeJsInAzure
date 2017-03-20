9cf76dcee61e09b476fc4b6eca7678d950dcb902
Cleyton Urani <CleytonUrani@users.noreply.github.com>
2017-03-20 16:22:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ var port = process.env.port || 1337;
 var express = require('express');
 var app = express();
 
+function resposta (res){
+  res.send(new Date());
+};
+
 app.get('/', function (req, res) {
   res.send(
     'Jquery node.js + Express'
   );
+  resposta(res);
 });
 
 app.get('/sobre', function (req, res) {
